a54a06d76c17b8f2c9419c9882e42247356d7270
Yuji Kawashima <kawasimayuji@gmail.com>
2017-01-18 19:22:40 +0900

    add Projects

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	[SerializeField]
+	private float speed = 1;
+
+	private float lateOnWoodPosition = 0;
+	private AudioSource source;
+
+	void Start(){
+		source = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis ("Vertical") * Time.deltaTime * speed);
+	}
+
+	void OnCollisionStay(Collision col){
+		if (col.gameObject.name == "Wood" && col.contacts[0].point.z - lateOnWoodPosition > 1f + Random.Range(-0.5f,0.5f) ) {
+			source.Play ();
+			lateOnWoodPosition = col.contacts [0].point.z;
+		}
+	}
+}
---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour
+{
+
+	[SerializeField]
+	private float Windpower;
+	[SerializeField]
+	private float scale;
+	[SerializeField]
+	private float power = 1;
+	[SerializeField]
+	private ParticleSystem windParticle;
+
+	private void Start()
+	{
+		StartCoroutine (WindBreathe ());
+	}
+
+	private IEnumerator WindBreathe()
+	{
+		AudioSource source = GetComponent<AudioSource> ();
+		while (true) {
+			Windpower = Mathf.PerlinNoise (Windpower * scale, Time.time * scale) * power;
+			foreach (GameObject ob in Object.FindObjectsOfType(typeof(GameObject))) {
+				if (ob.GetComponent<Rigidbody> ()) {
+					ob.GetComponent<Rigidbody> ().AddForce (Windpower, 0, 0, ForceMode.Force);
+				}
+			}
+			source.pitch = Windpower / (power * 2) + 1;
+			yield return null;
+		}
+	}
+}
