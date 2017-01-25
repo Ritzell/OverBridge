7b444e453b0a6f49558ff9b1bafd05b1dc470cbc
Yuji Kawashima <kawasimayuji@gmail.com>
2017-01-25 21:05:38 +0900

    WindForecastを芋づる式に更新されるようにした。アイコンの位置と大きなの更新を動的に行うようにした。

--- Assets/WindForecast.cs
+++ Assets/WindForecast.cs
@@ -7,14 +7,18 @@ public class WindForecast : MonoBehaviour {
 	private float _time = 0;
 	private float _power = 0;
 	public IEnumerator _nextState;
+	private WindForecastFrame frame;
 
 	[SerializeField]
 	private Text TimeText;
 	[SerializeField]
 	private Text PowerText;
 
-	void Start(){
+	void Awake(){
 		_nextState = NextState ();
+	}
+	void Start(){
+		frame = FindObjectOfType<WindForecastFrame> ();
 	}
 
 	public float time{
@@ -38,7 +42,6 @@ public class WindForecast : MonoBehaviour {
 	void DisplayTimeText(){
 		var time = Mathf.CeilToInt(_time);
 		TimeText.text = time.ToString ();
-		Debug.Log (time.ToString ());
 	}
 
 	void DisplyPowerText(){
@@ -46,12 +49,37 @@ public class WindForecast : MonoBehaviour {
 	}
 
 	public IEnumerator NextState(){
-		GetComponent<Image> ().color = new Color (193 / 255, 188 / 255, 255 / 255, 50);
+		StartCoroutine(ScaleUp(new Vector3 (0.8f, 0.8f, 0.8f)));
+		GetComponent<Image> ().color = new Color (193f / 255f, 188f / 255f, 255 / 255, 50f / 255f);
 		yield return null;
-		GetComponent<Image> ().color = new Color (193 / 255, 188 / 255, 255 / 255, 110);
+		StartCoroutine(ScaleUp(new Vector3 (0.9f, 0.9f, 0.9f)));
+		GetComponent<Image> ().color = new Color (193f / 255f, 188f / 255f, 255 / 255, 110f / 255f);
 		yield return null;
-		GetComponent<Image> ().color = new Color (193 / 255, 188 / 255, 255 / 255, 170);
+		StartCoroutine(ScaleUp(new Vector3 (1, 1, 1)));
+		GetComponent<Image> ().color = new Color (193f / 255f, 188f / 255f, 255 / 255, 170f / 255f);
+		StartCoroutine (destroy ());
+	}
+
+	private IEnumerator destroy(){
 		yield return new WaitForSeconds (_time);
+		frame.Next ();
 		Destroy (gameObject);
+	}
+
+	public IEnumerator MoveToPoint(Vector3 point){
+		RectTransform rect = GetComponent<RectTransform> ();
+		Vector3 startPosition = rect.anchoredPosition;
+		for (float i = 0; i < 1; i += Time.deltaTime * 2f) {
+			rect.anchoredPosition = Vector3.Lerp (startPosition, point, i);
+			yield return null;
+		}
+	}
+
+	private IEnumerator ScaleUp(Vector3 scale){
+		Vector3 startScale = transform.localScale;
+		for (float i = 0; i < 1; i += Time.deltaTime * 2f) {
+			transform.localScale = Vector3.Lerp (startScale, scale, i);
+			yield return null;
+		}
 	}
 }
--- Assets/WindForecastFrame.cs
+++ Assets/WindForecastFrame.cs
@@ -24,24 +24,26 @@ public class WindForecastFrame : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Next () {
-		Destroy (ForecastsObject [0]);
+	public void Next () {
+		ForecastsObject.RemoveAt (0);
 		for (int i = 0; i < 2; i++) {
-			ForecastsObject [i].transform.position = ForecastPosition [i];
+			StartCoroutine(ForecastsObject [i].GetComponent<WindForecast> ().MoveToPoint (ForecastPosition [i]));
+			//ForecastsObject [i].GetComponent<RectTransform> ().anchoredPosition= ForecastPosition [i];
 			ForecastsObject [i].GetComponent<WindForecast> ()._nextState.MoveNext();
 		}
 		Add (2);
 	}
 
 	void Add(int Number){
-		GameObject forecast = (GameObject)Instantiate (ForecastPrefab, canvas.transform);//ForecastPrefab,ForecastPosition[Number],Quaternion.identity);
+		GameObject forecast = (GameObject)Instantiate (ForecastPrefab, canvas.transform);
 		WindForecast forecastScript = forecast.GetComponent<WindForecast> ();
 		forecastScript.time = Forecasts [0].x;
 		forecastScript.power = Forecasts [0].y;
 		Forecasts.RemoveAt (0);
 		forecast.GetComponent<RectTransform> ().anchoredPosition = ForecastPosition [Number];
-		for (int i = 0; i < 3 - Number; i++) {
-			//forecastScript._nextState.MoveNext ();
+		ForecastsObject.Add (forecast);
+		for (int i = 0; i < 3-Number; i++) {
+			forecastScript._nextState.MoveNext ();
 		}
 	}
 }
